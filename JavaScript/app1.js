53d7859a2045dad56c200b861771e42e8f58c3ba
Hugo Barbosa da Silva <70847074+Hugo-BSilva@users.noreply.github.com>
2021-06-24 22:26:41 -0300

    ManipulandoWindow

---
+++ JavaScript/app1.js
@@ -0,0 +1,17 @@
+//console.log(window.innerWidth) imprime a largura da tela
+//window.location.href = 'https://google.com'
+//alert('Olá Filhote')
+//console.log(window.document)
+
+var input = document.getElementById('idade')
+var input1 = document.getElementsByClassName('idade')
+var input2 = document.getElementsByTagName('input')[0]
+var input3 = document.querySelector('div#app h1')
+var input4 = document.querySelector('div#app input.idade')
+var list = document.querySelector('div#app ul')
+console.log(input)
+console.log(input1)
+console.log(input2)
+console.log(input3)
+console.log(input4)
+console.log(list)
