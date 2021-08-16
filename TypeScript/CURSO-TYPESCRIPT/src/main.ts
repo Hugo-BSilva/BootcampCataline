b13afada6a881d510f4c72717060390f66ac6b79
hugo-bsilva <09silvahugo@gmail.com>
2021-08-16 20:46:59 -0300

    Update_Files_TypeScript

--- TypeScript/CURSO-TYPESCRIPT/src/main.ts
+++ TypeScript/CURSO-TYPESCRIPT/src/main.ts
@@ -106,3 +106,15 @@ console.log(sum(5,5))
 
 
 //DECORATORS
+function setApiVersion(apiVersion: string) {
+    return (constructor: any) => {
+        return class extends constructor{
+            version = apiVersion
+        }
+    }
+}
+
+@setApiVersion('1.0.0')
+class API {}
+
+console.log(new API())
